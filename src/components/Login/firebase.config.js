7db8f7f700703e6fcad1a2970bab4996d7aa2492
Rohit Kumar Datta <42374695+dattar321@users.noreply.github.com>
2021-03-29 14:56:01 +0600

    Delete firebase.config.js

--- src/components/Login/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCZalJychpXWAg4Z57kNCuDujVEmp7599E",
-  authDomain: "travel-guru-be90b.firebaseapp.com",
-  projectId: "travel-guru-be90b",
-  storageBucket: "travel-guru-be90b.appspot.com",
-  messagingSenderId: "312660735161",
-  appId: "1:312660735161:web:9eb549b6b9a88e7c619768"
-};
-export default firebaseConfig;
